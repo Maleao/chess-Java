f2bff07f91cdef026058e0b53aa0f08f02a49547
Maleao <mtdevjf@gmail.com>
2023-12-23 18:17:43 -0300

    Criando peças Rook e King

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -4,13 +4,14 @@ import src.boardgame.Board;
 import src.boardgame.Position;
 import src.chess.ChessMatch;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
-        ChessMatch chessMatch = new ChessMatch();
+       ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
-
+       UI.printBoard(chessMatch.getPieces());
 
     }
 }
